c4b65fb746d4578cb185767b7642a9932fca1505
Rafael Coelho <rafaellcoellho@gmail.com>
2018-10-09 16:50:57 -0300

    Cria os testes para conversão octal

--- src_tests/ep1Test.cpp
+++ src_tests/ep1Test.cpp
@@ -19,6 +19,10 @@ TEST_GROUP(ep1)
 	}
 };
 
+/*
+	Testes conversão binária
+*/
+
 TEST(ep1, convert_zero_to_bin)
 {
 	array = convert(0, 2);
@@ -53,4 +57,38 @@ TEST(ep1, convert_to_bin_periodical)
 {
 	array = convert(0.1, 2);
 	STRCMP_EQUAL("0.00011001100110011001", array);
+}
+
+/*
+	Testes conversão Octal
+*/
+
+TEST(ep1, convert_zero_to_octal)
+{
+	array = convert(0, 8);
+	STRCMP_EQUAL("0.0", array);
+}
+
+TEST(ep1, convert_to_octal_without_decimal)
+{
+	array = convert(10, 8);
+	STRCMP_EQUAL("12.0", array);
+}
+
+TEST(ep1, convert_to_octal_with_decimal_zero)
+{
+	array = convert(219.0, 8);
+	STRCMP_EQUAL("333.0", array);
+}
+
+TEST(ep1, convert_to_octal_with_decimal)
+{
+	array = convert(219.59375, 8);
+	STRCMP_EQUAL("333.46", array);
+}
+
+TEST(ep1, convert_to_octal_decimal_number)
+{
+	array = convert(0.359375, 8);
+	STRCMP_EQUAL("0.27", array);
 }
