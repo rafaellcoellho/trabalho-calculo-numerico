c5eed1aaebe77ecbf2fba44a176344fa12366f77
Rafael Coelho <rafaellcoellho@gmail.com>
2018-10-11 06:23:08 -0300

    Adicionando comentário da funçâo convert

--- src/ep1.c
+++ src/ep1.c
@@ -8,6 +8,8 @@
 
 char int_to_ascii[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
+
+// Corverte um numero do sistema de base 10 para outro
 char *convert(double number, int base)
 {
 	double num_int, num_frac;
@@ -48,6 +50,10 @@ char *convert(double number, int base)
 
 	return bin;
 }
+
+
+
+
 
 void menu(void)
 {
