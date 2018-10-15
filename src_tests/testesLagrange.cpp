8b1ad00367e0d35b7d5d337834a6b74487088039
Rafael Coelho <rafaellcoellho@gmail.com>
2018-10-15 20:43:28 -0300

    Troca ordem por grau nos testes

--- src_tests/testesLagrange.cpp
+++ src_tests/testesLagrange.cpp
@@ -22,33 +22,33 @@ TEST_GROUP(lagrange)
 
 TEST(lagrange, intervalo_positivo_de_raizes) {
     double polinomio[] = { 1, -5, -7, 29, 30 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo_positivo[2];
     double intervalo_negativo[2];
     
-    teorema_de_lagrange(polinomio, ordem, intervalo_positivo, intervalo_negativo);
+    teorema_de_lagrange(polinomio, grau, intervalo_positivo, intervalo_negativo);
     DOUBLES_EQUAL(0.6743, intervalo_positivo[0], 0.0001);
     DOUBLES_EQUAL(8, intervalo_positivo[1], 0.0001);
 }
 
 TEST(lagrange, intervalo_negativo_de_raizes) {
     double polinomio[] = { 1, -5, -7, 29, 30 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo_positivo[2];
     double intervalo_negativo[2];
     
-    teorema_de_lagrange(polinomio, ordem, intervalo_positivo, intervalo_negativo);
+    teorema_de_lagrange(polinomio, grau, intervalo_positivo, intervalo_negativo);
     DOUBLES_EQUAL(-6.3852, intervalo_negativo[0], 0.0001);
     DOUBLES_EQUAL(-0.5085, intervalo_negativo[1], 0.0001);
 }
 
 TEST(lagrange, limites_corretos_teorema_lagrange) {
     double polinomio[] = { 1, 5, -3, -7, -3 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo_positivo[2];
     double intervalo_negativo[2];
     
-    teorema_de_lagrange(polinomio, ordem, intervalo_positivo, intervalo_negativo);
+    teorema_de_lagrange(polinomio, grau, intervalo_positivo, intervalo_negativo);
     DOUBLES_EQUAL(0.457534, intervalo_positivo[0], 0.000001);
     DOUBLES_EQUAL(3.645751, intervalo_positivo[1], 0.000001);
     DOUBLES_EQUAL(-6.000000, intervalo_negativo[0], 0.000001);
@@ -57,36 +57,36 @@ TEST(lagrange, limites_corretos_teorema_lagrange) {
 
 TEST(lagrange, limites_incorretos_teorema_lagrange) {
     double polinomio[] = { 1, 5, 3, 7, 3 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo_positivo[2];
     double intervalo_negativo[2];
     
-    teorema_de_lagrange(polinomio, ordem, intervalo_positivo, intervalo_negativo);
+    teorema_de_lagrange(polinomio, grau, intervalo_positivo, intervalo_negativo);
     DOUBLES_EQUAL(0, intervalo_positivo[0], 0.000001);
     DOUBLES_EQUAL(0, intervalo_positivo[1], 0.000001);
 }
 
 TEST(lagrange, teorema_de_bolzano_intervalo_com_raiz) {
     double polinomio[] = { 1, 5, -3, -7, -3 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo[] = {1.0, 2.0};
     
-    CHECK_EQUAL(0 ,teorema_de_bolzano(polinomio, ordem, intervalo));
+    CHECK_EQUAL(0 ,teorema_de_bolzano(polinomio, grau, intervalo));
 }
 
 TEST(lagrange, teorema_de_bolzano_intervalo_sem_raiz) {
     double polinomio[] = { 1, 5, -3, -7, -3 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo[] = {2.0, 3.0};
     
-    CHECK_EQUAL(1 ,teorema_de_bolzano(polinomio, ordem, intervalo));
+    CHECK_EQUAL(1 ,teorema_de_bolzano(polinomio, grau, intervalo));
 }
 
 TEST(lagrange, raiz_aproximada_metodo_bissecao) {
     double polinomio[] = { 1, 5, -3, -7, -3 };
-    int ordem = 4;
+    int grau = 4;
     double intervalo[2] = {0.0 , 4.0};
 
-    double raiz_aproximada = metodo_da_bissecao(polinomio, ordem, intervalo);
+    double raiz_aproximada = metodo_da_bissecao(polinomio, grau, intervalo);
     DOUBLES_EQUAL(1.442585, raiz_aproximada, 0.000001);
 }
